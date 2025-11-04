af47a5597cb46c7dee86ebf6da9ee76351f021f5
Joseph Lopez <joseph.lpz1999@gmail.com>
2025-11-03 21:40:02 -0600

    Submit project

---
+++ src/main.js
@@ -0,0 +1,92 @@
+const dashboard = document.querySelector("#dashboard");
+const filteredButtons = document.querySelectorAll(".profile-card__button");
+
+let activities;
+let defaultButton;
+
+const getUserActivity = async () => {
+  const url = "src/data/data.json";
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
+
+const createCard = (activities, defaultTimeframe) => {
+  if (!dashboard) return;
+
+  activities.forEach((activity) => {
+    const article = document.createElement("article");
+    article.className = "activity-card";
+    article.innerHTML = `
+      <div class="activity-card__background activity-card__background--${
+        activity.title == "Self Care"
+          ? "self-care"
+          : activity.title.toLowerCase()
+      }">
+        <img
+          src="/src/images/icon-${
+            activity.title == "Self Care" ? "self-care" : activity.title
+          }.svg"
+          class="activity-card__icon"
+          alt="${activity.title} icon"
+        />
+      </div>
+      <div class="activity-card__content">
+        <div class="activity-card__header">
+          <p class="activity-card__name-activity">${activity.title}</p>
+          <button class="activity-card__button">
+            <img src="/src/images/icon-ellipsis.svg" alt="icon ellipsis" />
+          </button>
+        </div>
+        <div class="activity-card__timeframes">
+          <p class="activity-card__current">${
+            activity.timeframes[defaultTimeframe].current
+          }hrs</p>
+          <p class="activity-card__previus">Last Week - ${
+            activity.timeframes[defaultTimeframe].previous
+          }hrs</p>
+        </div>
+      </div>
+    `;
+
+    dashboard.appendChild(article);
+  });
+};
+
+const filterActivities = (timeframe) => {
+  document.querySelectorAll(".activity-card__timeframes").forEach((card, i) => {
+    card.children[0].textContent = `${
+      activities[i].timeframes[timeframe.id].current
+    }hrs`;
+    card.children[1].textContent = `Last Week - ${
+      activities[i].timeframes[timeframe.id].previous
+    }hrs`;
+  });
+};
+
+const loadActivities = async () => {
+  const defaultTimeframe = "weekly";
+  if (!activities) {
+    activities = await getUserActivity();
+    createCard(activities, defaultTimeframe);
+  }
+
+  filteredButtons.forEach((button) => {
+    if (button.id === defaultTimeframe) {
+      button.classList.add("profile-card__button--active");
+      defaultButton = button;
+    }
+
+    button.addEventListener("click", (e) => {
+      filterActivities(e.target);
+      const target = e.target;
+      if (target.id !== defaultButton.id) {
+        defaultButton.classList.remove("profile-card__button--active");
+        target.classList.add("profile-card__button--active");
+        defaultButton = target;
+      }
+    });
+  });
+};
+
+loadActivities();
